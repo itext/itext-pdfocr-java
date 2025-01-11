0fab639b6cd5194ec4fbc32efa658ab15aadca6c
Vitali Prudnikovich <vitali.prudnikovich@apryse.com>
2025-01-11 09:15:01 +0300

    Update copyright year to 2025
    DEVSIX-8055

--- pdfocr-tesseract4/src/main/java/com/itextpdf/pdfocr/tesseract4/actions/data/PdfOcrTesseract4ProductData.java
+++ pdfocr-tesseract4/src/main/java/com/itextpdf/pdfocr/tesseract4/actions/data/PdfOcrTesseract4ProductData.java
@@ -1,6 +1,6 @@
 /*
     This file is part of the iText (R) project.
-    Copyright (c) 1998-2024 Apryse Group NV
+    Copyright (c) 1998-2025 Apryse Group NV
     Authors: Apryse Software.
 
     This program is offered under a commercial and under the AGPL license.
@@ -32,7 +32,7 @@ public class PdfOcrTesseract4ProductData {
     private static final String PDF_OCR_TESSERACT4_PUBLIC_PRODUCT_NAME = "pdfOCR-Tesseract4";
     private static final String PDF_OCR_VERSION = "4.0.1-SNAPSHOT";
     private static final int PDF_OCR_COPYRIGHT_SINCE = 2000;
-    private static final int PDF_OCR_COPYRIGHT_TO = 2024;
+    private static final int PDF_OCR_COPYRIGHT_TO = 2025;
 
     private static final ProductData PDF_OCR_PRODUCT_DATA = new ProductData(PDF_OCR_TESSERACT4_PUBLIC_PRODUCT_NAME,
             PDF_OCR_TESSERACT4_PRODUCT_NAME, PDF_OCR_VERSION, PDF_OCR_COPYRIGHT_SINCE, PDF_OCR_COPYRIGHT_TO);
