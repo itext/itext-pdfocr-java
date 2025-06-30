d8339cebb3dd478f69a1672294dc9225d947476e
Dmitry Radchuk <dmitry.radchuk@apryse.com>
2025-06-30 05:02:40 +0300

    Switch to ExtendedMappingConfiguration

--- pdfocr-api/src/sharpenconfig/java/com/itextpdf/pdfocr/SharpenConfigMapping.java
+++ pdfocr-api/src/sharpenconfig/java/com/itextpdf/pdfocr/SharpenConfigMapping.java
@@ -26,13 +26,14 @@ import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import sharpen.config.MappingConfiguration;
+
+import sharpen.config.ExtendedMappingConfiguration;
 import sharpen.config.MappingConfigurator;
 import sharpen.config.ModuleOption;
 import sharpen.config.ModulesConfigurator;
 import sharpen.config.OptionsConfigurator;
 
-public class SharpenConfigMapping implements MappingConfiguration {
+public class SharpenConfigMapping implements ExtendedMappingConfiguration {
 
     @Override
     public int getMappingPriority() {
@@ -55,6 +56,11 @@ public class SharpenConfigMapping implements MappingConfiguration {
         // images
         configurator.addFullName("System.Drawing.Bitmap");
         configurator.ignoreUsing("Javax.Imageio");
+    }
+
+    @Override
+    public void setConfigModuleSettings(ModulesConfigurator modulesConfigurator) {
+
     }
 
     @Override
