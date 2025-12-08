28e8cfe030218581344b23c88352523c53d116ba
Vlad Lipskiy <vlad.lipskiy@apryse.com>
2025-12-08 17:32:37 +0300

    Add post-processor for EasyOCR text detection models
    The post-processing there is, for the most part, also similar to what
    we have now, with a couple of exceptions:
    
    * The model returns two channels instead of one. The first one detect
      the text glyphs themselves. While the seconds one outputs
      "connections" between the glyphs.
    * The mask for finding text contours is based on the sum of both
      channels instead of just the first, text prediction, one.
    * Text score calculation is based on `max` instead of `mean`.
    * Text box enlargement is calculated differently.
    * Text boxes are merged into lines at the end to pass to recognition.
    
    Since you can use the PaddleOCR detection model to get lines, I am not
    quite sure, if there is a practical need for this. But it works. Maybe
    it could be useful for some of the more esoteric languages.

---
+++ pdfocr-onnxtr/src/main/java/com/itextpdf/pdfocr/onnxtr/detection/EasyOcrDetectionPostProcessor.java
@@ -0,0 +1,118 @@
+/*
+    Copyright (c) 2020 JaidedAI Authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+package com.itextpdf.pdfocr.onnxtr.detection;
+
+import com.itextpdf.kernel.geom.Point;
+import com.itextpdf.pdfocr.onnxtr.FloatBufferMdArray;
+import com.itextpdf.pdfocr.onnxtr.detection.score.IScoreCalculator;
+import com.itextpdf.pdfocr.onnxtr.detection.score.MaxScoreCalculator;
+import com.itextpdf.pdfocr.onnxtr.merging.EasyOcrTextBoxMerger;
+
+import java.awt.image.BufferedImage;
+import java.nio.FloatBuffer;
+import java.util.List;
+
+/**
+ * Implementation of a text detection predictor post-processor, used for
+ * EasyOCR model outputs.
+ */
+public class EasyOcrDetectionPostProcessor extends BasicDetectionPostProcessor {
+    /**
+     * Threshold for binarization of the text score array.
+     */
+    private static final float TEXT_BINARIZATION_THRESHOLD = 0.4F;
+    /**
+     * Threshold for binarization of the link score array.
+     */
+    private static final float LINK_BINARIZATION_THRESHOLD = 0.4F;
+
+    /**
+     * Creates a new post-processor.
+     *
+     * @param scoreThreshold score threshold for a detected box. If score is lower than this value,
+     *                       the box gets discarded
+     */
+    public EasyOcrDetectionPostProcessor(float scoreThreshold) {
+        super(1.0F, scoreThreshold, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Creates a new post-processor with the default parameters.
+     */
+    public EasyOcrDetectionPostProcessor() {
+        this(0.7F);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Point[]> process(BufferedImage input, FloatBufferMdArray output) {
+        /*
+         * The text detection model from EasyOCR, for the most part, returns
+         * words or small groups of words. Since the EasyOCR text recognition
+         * models expect lines as input, we need to merge the boxes.
+         */
+        return new EasyOcrTextBoxMerger().process(super.process(input, output));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected FloatBufferMdArray getMaskSourceArray(FloatBufferMdArray output) {
+        /*
+         * Mask for finding contours is based on there being either text or
+         * link data. So we are creating a new buffer, where they are
+         * combined.
+         */
+        final FloatBuffer textScoreBuffer = output.getSubArray(0).getData();
+        final FloatBuffer linkScoreBuffer = output.getSubArray(1).getData();
+        final int height = output.getDimension(1);
+        final int width = output.getDimension(2);
+        final int size = height * width;
+        final FloatBuffer maskSourceBuffer = FloatBuffer.allocate(height * width);
+        for (int i = 0; i < size; ++i) {
+            final float text = textScoreBuffer.get() >= TEXT_BINARIZATION_THRESHOLD ? 1.0F : 0.0F;
+            final float link = linkScoreBuffer.get() >= LINK_BINARIZATION_THRESHOLD ? 1.0F : 0.0F;
+            maskSourceBuffer.put(text + link);
+        }
+        maskSourceBuffer.rewind();
+        return new FloatBufferMdArray(maskSourceBuffer, new long[]{height, width});
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected IScoreCalculator createScoreCalculator() {
+        return new MaxScoreCalculator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected double calcTextBoxEnlargement(double width, double height) {
+        /*
+         * In EasyOCR it is calculated as `2 * sqrt(area / max(w, h))`. But in
+         * their case they use the area of the contour. Since we are not
+         * calculating that, but are working on a box instead, it got
+         * simplified further.
+         */
+        return 2.0 * Math.sqrt(Math.min(width, height));
+    }
+}
