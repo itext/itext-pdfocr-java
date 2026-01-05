6fe7ef53cbe6b6e4c4c7b6525bc13ef922df8056
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2026-01-05 18:04:31 +0300

    Update copyright year to 2026
    DEVSIX-8834

--- pdfocr-onnxtr/src/main/java/com/itextpdf/pdfocr/onnxtr/actions/data/PdfOcrOnnxTrProductData.java
+++ pdfocr-onnxtr/src/main/java/com/itextpdf/pdfocr/onnxtr/actions/data/PdfOcrOnnxTrProductData.java
@@ -1,6 +1,6 @@
 /*
     This file is part of the iText (R) project.
-    Copyright (c) 1998-2025 Apryse Group NV
+    Copyright (c) 1998-2026 Apryse Group NV
     Authors: Apryse Software.
 
     This program is offered under a commercial and under the AGPL license.
@@ -32,7 +32,7 @@ public final class PdfOcrOnnxTrProductData {
     private static final String PDF_OCR_ONNXTR_PUBLIC_PRODUCT_NAME = "pdfOCR-OnnxTR";
     private static final String PDF_OCR_VERSION = "4.2.0-SNAPSHOT";
     private static final int PDF_OCR_COPYRIGHT_SINCE = 2000;
-    private static final int PDF_OCR_COPYRIGHT_TO = 2025;
+    private static final int PDF_OCR_COPYRIGHT_TO = 2026;
 
     private static final ProductData PDF_OCR_ONNXTR_PRODUCT_DATA = new ProductData(PDF_OCR_ONNXTR_PUBLIC_PRODUCT_NAME,
             PDF_OCR_ONNXTR_PRODUCT_NAME, PDF_OCR_VERSION, PDF_OCR_COPYRIGHT_SINCE, PDF_OCR_COPYRIGHT_TO);
