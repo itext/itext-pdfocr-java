47969721af22756ed81e3c9fcc6d2bbb7f695f2f
Dmitry Chubrick <dmitry.chubrick@apryse.com>
2026-02-23 15:39:56 +0300

    Get rid of "tr" in module names and rename pdfocr-onnxtr -> pdfocr-onnx-cpu
    DEVSIX-9740

---
+++ pdfocr-onnx-abstract/src/sharpenconfig/java/com/itextpdf/pdfocr/onnx/SharpenConfigMapping.java
@@ -0,0 +1,192 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2026 Apryse Group NV
+    Authors: Apryse Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.pdfocr.onnx;
+
+import sharpen.config.MappingConfiguration;
+import sharpen.config.MappingConfigurator;
+import sharpen.config.ModuleOption;
+import sharpen.config.ModulesConfigurator;
+import sharpen.config.OptionsConfigurator;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Service implementation of {@link sharpen.config.MappingConfiguration} containing the module's Sharpen configuration.
+ */
+public class SharpenConfigMapping implements MappingConfiguration {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getMappingPriority() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getModuleName() {
+        return "pdfocr-onnx-abstract";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void applyMappingConfiguration(MappingConfigurator configurator) {
+        configurator.addCustomUsingForMethodInvocation("java.awt.image.BufferedImage",
+                Collections.singletonList("iText.Pdfocr.Onnxtr.Util"));
+        configurator.mapMethod("java.awt.image.BufferedImage.getWidth", "BufferedImageUtil.GetWidth");
+        configurator.mapMethod("java.awt.image.BufferedImage.getHeight", "BufferedImageUtil.GetHeight");
+        configurator.mapMethod("javax.imageio.ImageIO.read", "IronSoftware.Drawing.AnyBitmap.FromFile");
+        configurator.mapMethodWithParameterConversion("javax.imageio.ImageIO.read", "1:memberCall:FullName");
+
+        configurator.mapMethod("java.util.Collection.iterator", "GetEnumerator");
+        configurator.mapMethod("java.util.Iterator.hasNext", "MoveNext");
+        configurator.mapProperty("java.util.Iterator.next", "Current");
+        mapOpenCv(configurator);
+
+        configurator.mapMethod("org.junit.jupiter.api.Assertions.assertDoesNotThrow",
+                "NUnit.Framework.Assert.DoesNotThrow", false);
+        configurator.mapMethod("org.junit.jupiter.api.Assertions.assertInstanceOf",
+                "NUnit.Framework.Assert.IsInstanceOf");
+
+        configurator.mapType("java.awt.Color", "IronSoftware.Drawing.Color");
+        configurator.mapField("java.awt.Color.BLACK", "IronSoftware.Drawing.Color.Black");
+        configurator.mapField("java.awt.Color.GRAY", "IronSoftware.Drawing.Color.Gray");
+        configurator.mapField("java.awt.Color.WHITE", "IronSoftware.Drawing.Color.White");
+
+        configurator.ignoreUsing("Org.Bytedeco.Opencv.Opencv_core");
+        configurator.addCustomUsingDeclaration("com.itextpdf.pdfocr.onnxtr.util.YamlUtilTest",
+                Arrays.asList("System.Linq"));
+    }
+
+    private void mapOpenCv(MappingConfigurator configurator) {
+        configurator.addFullName("iText.Kernel.Geom.Point");
+        configurator.mapType("org.bytedeco.opencv.opencv_core.Mat", "OpenCvSharp.Mat");
+        configurator.mapType("org.bytedeco.opencv.opencv_core.MatVector", "OpenCvSharp.Internal.Vectors.VectorOfMat");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.MatVector.size", "Size");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.RotatedRect.size", "Size");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.RotatedRect.angle", "Angle");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Size2f.width", "Width");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Size2f.height", "Height");
+        configurator.mapMethod("java.nio.FloatBuffer.array", "");
+
+        configurator.removeMethod("org.bytedeco.javacpp.Pointer.close");
+
+        configurator.mapMethod("org.bytedeco.opencv.opencv_core.Size2f.close", "");
+        configurator.mapType("ai.onnxruntime.NodeInfo", "Microsoft.ML.OnnxRuntime.NodeMetadata");
+        configurator.mapType("ai.onnxruntime.OnnxValue", "Microsoft.ML.OnnxRuntime.DisposableNamedOnnxValue");
+        configurator.mapType("ai.onnxruntime.TensorInfo", "Microsoft.ML.OnnxRuntime.Tensors.Tensor");
+        configurator.mapType("org.bytedeco.opencv.opencv_core.Size2f", "OpenCvSharp.Size2f");
+        configurator.mapType("org.bytedeco.opencv.opencv_core.RotatedRect", "OpenCvSharp.RotatedRect");
+        configurator.mapType("org.bytedeco.opencv.global.opencv_imgproc", "OpenCvSharp.Cv2");
+        configurator.mapField("org.bytedeco.opencv.global.opencv_imgproc.MORPH_OPEN", "OpenCvSharp.MorphTypes.Open");
+        configurator.mapField("org.bytedeco.opencv.global.opencv_imgproc.RETR_EXTERNAL", "OpenCvSharp.RetrievalModes.External");
+        configurator.mapField("org.bytedeco.opencv.global.opencv_imgproc.CHAIN_APPROX_SIMPLE", "OpenCvSharp.ContourApproximationModes.ApproxSimple");
+        configurator.mapIndexer("org.bytedeco.opencv.opencv_core.MatVector.get");
+        configurator.mapType("org.opencv.core.CvType", "OpenCvSharp.MatType");
+
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Rect.x", "X");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Rect.y", "Y");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Rect.height", "Height");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Rect.width", "Width");
+        configurator.mapField("org.bytedeco.opencv.opencv_core.AbstractScalar.WHITE", "OpenCvSharp.Scalar.White");
+        configurator.mapField("org.bytedeco.opencv.opencv_core.AbstractScalar.ZERO", "OpenCvSharp.Scalar.Black");
+        configurator.mapType("org.bytedeco.opencv.opencv_core.Scalar", "OpenCvSharp.Scalar");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Mat.rows()", "Rows");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Mat.cols()", "Cols");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Point2f.x()", "X");
+        configurator.mapProperty("org.bytedeco.opencv.opencv_core.Point2f.y()", "Y");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void applySharpenOptions(OptionsConfigurator configurator) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void applyConfigModuleSettings(ModulesConfigurator configurator) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setConfigModuleSettings(ModulesConfigurator modulesConfigurator) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<ModuleOption> getAvailableModuleSettings() {
+        return Collections.EMPTY_SET;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<String> getDependencies() {
+        return Collections.EMPTY_LIST;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<String> getIgnoredSourceFiles() {
+        return Collections.EMPTY_LIST;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<String> getIgnoredResources() {
+        return Collections.EMPTY_LIST;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<SimpleImmutableEntry<String, String>> getOverwrittenResources() {
+        return Collections.EMPTY_LIST;
+    }
+}
---
+++ pdfocr-onnx-cpu/src/test/java/com/itextpdf/pdfocr/onnx/cpu/IntegrationPdfOcrOnnxTest.java
@@ -0,0 +1,159 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2026 Apryse Group NV
+    Authors: Apryse Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.pdfocr.onnx.cpu;
+
+import com.itextpdf.kernel.colors.DeviceCmyk;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.utils.CompareTool;
+import com.itextpdf.pdfocr.IOcrEngine;
+import com.itextpdf.pdfocr.OcrPdfCreator;
+import com.itextpdf.pdfocr.OcrPdfCreatorProperties;
+import com.itextpdf.pdfocr.TextInfo;
+import com.itextpdf.pdfocr.onnxtr.OnnxTrEngineProperties;
+import com.itextpdf.pdfocr.onnxtr.OnnxTrOcrEngine;
+import com.itextpdf.pdfocr.onnxtr.TextPositioning;
+import com.itextpdf.pdfocr.onnxtr.detection.IDetectionPredictor;
+import com.itextpdf.pdfocr.onnxtr.detection.OnnxDetectionPredictor;
+import com.itextpdf.pdfocr.onnxtr.orientation.IOrientationPredictor;
+import com.itextpdf.pdfocr.onnxtr.orientation.OnnxOrientationPredictor;
+import com.itextpdf.pdfocr.onnxtr.recognition.IRecognitionPredictor;
+import com.itextpdf.pdfocr.onnxtr.recognition.OnnxRecognitionPredictor;
+import com.itextpdf.test.ExtendedITextTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("IntegrationTest")
+public class IntegrationPdfOcrOnnxTest extends ExtendedITextTest {
+    private static final String TEST_DIRECTORY = "./src/test/resources/com/itextpdf/pdfocr/";
+    private static final String TEST_IMAGE_DIRECTORY = TEST_DIRECTORY + "images/";
+    private static final String TEST_PDFS_DIRECTORY = TEST_DIRECTORY + "pdfs/";
+
+    private static final String SOURCE_FOLDER = TEST_DIRECTORY + "IntegrationPdfOcrOnnxTest/";
+    private static final String DESTINATION_FOLDER = "./target/test/resources/com/itextpdf/pdfocr/IntegrationPdfOcrOnnxTest/";
+
+    private static final String FAST = TEST_DIRECTORY + "models/rep_fast_tiny-28867779.onnx";
+    private static final String CRNNVGG16 = TEST_DIRECTORY + "models/crnn_vgg16_bn-662979cc.onnx";
+    private static final String MOBILENETV3 = TEST_DIRECTORY + "models/mobilenet_v3_small_crop_orientation-5620cf7e.onnx";
+
+    private static OnnxTrOcrEngine OCR_ENGINE_MAKE_PDF_SEARCHABLE;
+    private static OnnxTrOcrEngine OCR_ENGINE_IMAGE_OCR;
+    private static OnnxTrOcrEngine OCR_ENGINE_CREATE_PDF;
+
+    @BeforeAll
+    public static void beforeClass() {
+        createOrClearDestinationFolder(DESTINATION_FOLDER);
+
+        IDetectionPredictor detectionPredictor = OnnxDetectionPredictor.fast(FAST);
+        IOrientationPredictor orientationPredictor = OnnxOrientationPredictor.mobileNetV3(MOBILENETV3);
+        IRecognitionPredictor recognitionPredictor = OnnxRecognitionPredictor.crnnVgg16(CRNNVGG16);
+
+        OCR_ENGINE_MAKE_PDF_SEARCHABLE = new OnnxTrOcrEngine(detectionPredictor, orientationPredictor,
+                recognitionPredictor);
+
+        OCR_ENGINE_IMAGE_OCR = new OnnxTrOcrEngine(detectionPredictor, null, recognitionPredictor,
+                new OnnxTrEngineProperties().setTextPositioning(TextPositioning.BY_WORDS));
+
+        OCR_ENGINE_CREATE_PDF = new OnnxTrOcrEngine(detectionPredictor, recognitionPredictor);
+    }
+
+    @AfterAll
+    public static void afterClass() throws Exception {
+        OCR_ENGINE_MAKE_PDF_SEARCHABLE.close();
+        OCR_ENGINE_IMAGE_OCR.close();
+        OCR_ENGINE_CREATE_PDF.close();
+    }
+
+    @Test
+    public void makePdfSearchableBasicTest() throws IOException, InterruptedException {
+        makePdfSearchable("numbers");
+    }
+
+    @Test
+    public void makePdfSearchablePageRotatedTest() throws IOException, InterruptedException {
+        makePdfSearchable("pageRotation");
+    }
+
+    @Test
+    public void imageOcrBasicTest() {
+        String src = TEST_IMAGE_DIRECTORY + "example_01.BMP";
+        File imageFile = new File(src);
+
+        String textFromImage = getTextFromImage(imageFile, OCR_ENGINE_IMAGE_OCR);
+        Assertions.assertEquals("Ihis\n1S\na\ntest\nmessage\n-\nfor\nOCR\nScanner\nTest\n", textFromImage);
+    }
+
+    @Test
+    public void createPdfBasicTest() throws IOException, InterruptedException {
+        String src = TEST_IMAGE_DIRECTORY + "example_04.png";
+        String dest = DESTINATION_FOLDER + "basicTest.pdf";
+        String cmp = SOURCE_FOLDER + "cmp_basicTest.pdf";
+
+        doOcrAndCreatePdf(src, dest);
+        Assertions.assertNull(new CompareTool().compareByContent(dest, cmp, DESTINATION_FOLDER, "diff_"));
+    }
+
+    private static void doOcrAndCreatePdf(String imagePath, String destPdfPath) throws IOException {
+        OcrPdfCreator ocrPdfCreator = new OcrPdfCreator(OCR_ENGINE_CREATE_PDF);
+        try (PdfWriter writer = new PdfWriter(destPdfPath)) {
+            ocrPdfCreator.createPdf(Collections.singletonList(new File(imagePath)), writer).close();
+        }
+    }
+
+    private static String getTextFromImage(File imageFile, IOcrEngine ocrEngine) {
+        Map<Integer, List<TextInfo>> integerListMap = ocrEngine.doImageOcr(imageFile);
+        return getStringFromListMap(integerListMap);
+    }
+
+    private static String getStringFromListMap(Map<Integer, List<TextInfo>> listMap) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Entry<Integer, List<TextInfo>> entry : listMap.entrySet()) {
+            for (TextInfo textInfo : entry.getValue()) {
+                if (textInfo.getText() != null) {
+                    stringBuilder.append(textInfo.getText()).append('\n');
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private void makePdfSearchable(String fileName) throws IOException, InterruptedException {
+        String srcPath = TEST_PDFS_DIRECTORY + fileName + ".pdf";
+        String outPath = DESTINATION_FOLDER + fileName + ".pdf";
+        String cmpPath = SOURCE_FOLDER + "cmp_" + fileName + ".pdf";
+
+        OcrPdfCreator ocrPdfCreator = new OcrPdfCreator(OCR_ENGINE_MAKE_PDF_SEARCHABLE,
+                new OcrPdfCreatorProperties().setTextColor(DeviceCmyk.MAGENTA));
+        ocrPdfCreator.makePdfSearchable(new File(srcPath), new File(outPath));
+        Assertions.assertNull(new CompareTool().compareByContent(outPath, cmpPath, DESTINATION_FOLDER, "diff_"));
+    }
+}
